5626b9af509c6a7995841117d24363e75fd6ebc4
Shaine Hatch <shaine@squidtree.com>
2017-06-30 17:01:00 -0600

    Init

---
+++ test/index-test.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+
+describe('Personable reporter demo', () => {
+    it('shows a success message', () => {
+        expect(true).to.be.true;
+    });
+
+    it('shows a first error message', () => {
+        expect(false).to.be.true;
+    });
+
+    it('shows a second error message', () => {
+        expect(false).to.be.true;
+    });
+
+    it('shows a third error with diffs', () => {
+        expect({
+            a: 2,
+            b: 3,
+            c: 4
+        }).to.eql({
+            a: 2,
+            b: 4,
+            d: 5
+        });
+    });
+
+    it('shows a pending error message');
+});
